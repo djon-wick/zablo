c5719331d142ebd1fb90108f76d8d40f4b694831
djon-wick <50735200+djon-wick@users.noreply.github.com>
2019-06-04 12:53:25 +0300

    Update rh.h

--- tests/rh.h
+++ tests/rh.h
@@ -13,7 +13,7 @@ TEST(rh1, t1)
     text txt = create_text();
     FILE *f;
     char buf[MAXLINE + 1];
-    char filepath[] = "D:/projects/task2/build-qmake-gtest-Desktop_Qt_5_12_0_MinGW_64_bit-Debug/app/tuda";
+    char filepath[] = "build-qmake-gtest-Desktop_Qt_5_12_0_MinGW_64_bit-Debug/app/tuda";
 
     load(txt, filepath);
 
@@ -24,7 +24,7 @@ TEST(rh1, t1)
     int index = 0, count = 0;
 
     /* Открываем файл для чтения, контролируя ошибки */
-    if ((f = fopen("D:/projects/task2/build-qmake-gtest-Desktop_Qt_5_12_0_MinGW_64_bit-Debug/app/tuda", "r")) == NULL) {
+    if ((f = fopen("build-qmake-gtest-Desktop_Qt_5_12_0_MinGW_64_bit-Debug/app/tuda", "r")) == NULL) {
         FAIL();
     }
 
