e849fd7ed39aecad45becfd8a963358a07eaadfe
djon-wick <50735200+djon-wick@users.noreply.github.com>
2019-06-04 12:53:44 +0300

    Update shownum.h

--- tests/shownum.h
+++ tests/shownum.h
@@ -13,7 +13,7 @@ TEST(shownum1, t1)
     text txt = create_text();
     FILE *f;
     char buf[MAXLINE + 1];
-    char filepath[] = "D:/task2/build-qmake-gtest-Desktop_Qt_5_12_0_MinGW_64_bit-Debug/app/tuda";
+    char filepath[] = "build-qmake-gtest-Desktop_Qt_5_12_0_MinGW_64_bit-Debug/app/tuda";
 
     load(txt, filepath);
 
