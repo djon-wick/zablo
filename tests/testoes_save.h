e564def0431b48a37daf6bd33c6e5abdf85b6c2d
djon-wick <50735200+djon-wick@users.noreply.github.com>
2019-06-04 12:56:16 +0300

    Update testoes_save.h

--- tests/testoes_save.h
+++ tests/testoes_save.h
@@ -13,7 +13,7 @@ TEST(save1, t1)
     text txt = create_text();
     FILE *f;
     char buf[MAXLINE + 1];
-    char filepath[] = "D:/projects/task2/build-qmake-gtest-Desktop_Qt_5_12_0_MinGW_64_bit-Debug/app/tuda";
+    char filepath[] = "build-qmake-gtest-Desktop_Qt_5_12_0_MinGW_64_bit-Debug/app/tuda";
     char kaput[] = "kaput";
 
     load(txt, filepath);
@@ -22,7 +22,7 @@ TEST(save1, t1)
     save(txt, kaput);
 
     /* Открываем файл для чтения, контролируя ошибки */
-    if ((f = fopen("D:/projects/task2/build-qmake-gtest-Desktop_Qt_5_12_0_MinGW_64_bit-Debug/tests/kaput", "r")) == NULL) {
+    if ((f = fopen("build-qmake-gtest-Desktop_Qt_5_12_0_MinGW_64_bit-Debug/tests/kaput", "r")) == NULL) {
         FAIL();
     }
 
